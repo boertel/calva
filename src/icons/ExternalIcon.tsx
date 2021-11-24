2ad3c32768cc876ead35b6156fd34d0f0d46df62
boertel <ben@comediadesign.com>
2021-11-24 13:06:46 -0800

    update External meeting icon

--- src/icons/ExternalIcon.tsx
+++ src/icons/ExternalIcon.tsx
@@ -3,19 +3,29 @@ import * as React from "react";
 export function ExternalIcon({ size = "1em", ...props }: { size: string } & React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
       width={size}
       height={size}
-      fill="none"
+      strokeWidth={1.5}
       viewBox="0 0 24 24"
-      stroke="currentColor"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
+        d="M12 13a1 1 0 100-2 1 1 0 000 2z"
+        fill="currentColor"
+        stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth={2}
-        d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        strokeWidth="3"
+      />
+      <path
+        d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeWidth="2"
+        strokeLinejoin="round"
+        strokeDasharray="3 6"
       />
     </svg>
   );
