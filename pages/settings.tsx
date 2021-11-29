3d8e06f867895f29a6f70166bee912c7cefaa1c7
Benjamin Oertel <benjamin.oertel@gmail.com>
2021-11-29 10:32:43 -0800

    Add a quick settings page (#4)

---
+++ pages/settings.tsx
@@ -0,0 +1,29 @@
+import UserMenu from "components/UserMenu";
+import { useUser } from "@/hooks";
+
+export default function Settings() {
+  const user = useUser();
+  return (
+    <>
+      <main className="flex flex-col max-w-prose w-full mx-auto space-y-8 mt-6">
+        <h1 className="text-5xl font-black">Settings</h1>
+        <section>
+          <h3 className="font-bold text-md">
+            Connected with: <small className="font-normal text-base">{user?.email}</small>
+          </h3>
+        </section>
+        <section>
+          <form action="/api/auth/logout" method="post">
+            <button
+              type="submit"
+              className="px-4 py-2 rounded-md border border-white bg-white bg-opacity-0 hover:bg-opacity-20"
+            >
+              Log out
+            </button>
+          </form>
+        </section>
+      </main>
+      <UserMenu href="/" />
+    </>
+  );
+}
