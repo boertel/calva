2b63d5b4194f7701922f405ee93d8aef472a95db
boertel <ben@comediadesign.com>
2022-01-13 14:45:06 -0800

    wrong padding

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -95,7 +95,7 @@ export function TodoList() {
   return (
     <ul className="pt-6 pb-4">
       {showOnlyTodo && (
-        <li className="flex items-center px-5 text-green-500 pt-2">
+        <li className="flex items-center px-5 text-green-500 pb-2">
           <TodoDoneIcon className="w-5 h-5 mr-3" />
           {numberOfTasksDone} task{numberOfTasksDone !== 1 && "s"} done. Keep it up! 🎉
         </li>
