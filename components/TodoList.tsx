77252ad7406573c3f3c11d5dc2e263140c119b6d
boertel <ben@comediadesign.com>
2022-01-13 14:40:40 -0800

    quick summary when hiding tasks

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -88,8 +88,18 @@ export function TodoList() {
   }, [setPlaceholder]);
 
   const key = dayjs().format("YYYY-MM-DD");
+
+  const numberOfTasksDone = todos.filter(
+    ({ date, status }: Todo) => dayjs.utc(date).format("YYYY-MM-DD") === key && status === "done"
+  ).length;
   return (
     <ul className="pt-6 pb-4">
+      {showOnlyTodo && (
+        <li className="flex items-center px-5 text-green-500 pt-2">
+          <TodoDoneIcon className="w-5 h-5 mr-3" />
+          {numberOfTasksDone} task{numberOfTasksDone !== 1 && "s"} done. Keep it up! 🎉
+        </li>
+      )}
       {todos
         .filter(({ date, status }: Todo) =>
           showOnlyTodo ? status === "todo" : dayjs.utc(date).format("YYYY-MM-DD") === key
