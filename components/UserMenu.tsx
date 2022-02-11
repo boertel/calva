8bcf60763216eafe1e6909cc9c894b703f4d63d0
boertel <ben@comediadesign.com>
2022-02-10 19:30:22 -0800

    increment zindex for user menu

--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -1,11 +1,11 @@
+import { useUser } from "@/hooks";
 import Image from "next/image";
 import Link from "next/link";
-import { useUser } from "@/hooks";
 
 export default function UserMenu({ href }: { href: string }) {
   const user = useUser();
   return (
-    <div className="fixed bottom-0 left-0 mb-6 ml-8 hidden md:block">
+    <div className="fixed bottom-0 left-0 mb-6 ml-8 hidden md:block z-20">
       {!!user?.image && (
         <Link href={href}>
           <a className="flex w-12 h-12 hover:ring-2 rounded-full hover:ring-purple-500 border-4 border-transparent cursor-pointer z-[-1]">
